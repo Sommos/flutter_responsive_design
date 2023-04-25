2370618b9e38c614ad36c575b851cf11acbd5c4b
Sommos <Sommosgaming@gmail.com>
2023-04-25 12:12:02 +0100

    added desktop UI

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import 'util/my_box.dart';
+import 'util/my_tile.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
@@ -20,6 +22,45 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
           // open drawer automatically on desktop
           myDrawer,
           // rest of body
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                // 4 boxes on top
+                AspectRatio(
+                  aspectRatio: 4.00,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                      ),
+                      itemBuilder: (context, index) {
+                        return const MyBox();
+                      },
+                    ),
+                  ),
+                ),
+                // tiles below it
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 7,
+                    itemBuilder: (context, index) {
+                      return const MyTile();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(child: Container(color: Colors.pink)),
+              ],
+            ),
+          ),
         ],
       ),
     );
